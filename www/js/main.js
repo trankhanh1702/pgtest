e86eca8e913771af2e3a7d9b8629712249dba6a1
trankhanh1702 <trankhanh1702@gmail.com>
2013-09-29 20:40:32 +0700

    Fix all swipe events

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,5 @@
-$("#home-slider").click(function() {
+var menuHandler = function() {
 	var sliderTarget = $("#left-menu");
-
 	if(sliderTarget.data("clickState") != 1) {
 		sliderTarget.data("clickState", 1).
 			removeClass('collapse').
@@ -17,17 +16,43 @@ $("#home-slider").click(function() {
 				removeClass("custom-slider");
 		});
 	};
+}
 
-})
+$("#home-slider").click(menuHandler)
 
-$(function(){
-  // Bind the swipeleftHandler callback function to the swipe event on div.box
-  $( ".container" ).on( "swipeleft", swipeleftHandler );
- 
-  // Callback function references the event target and adds the 'swipeleft' class to it
-  function swipeleftHandler( event ){
-  	// alert("")
-  	$(".container").css("color", "red")
-    // $( event.target ).addClass( "swipeleft" );
-  }
-});
+// Callback function references the event target and adds the 'swipeleft' class to it
+function swiperightHandler(event){
+
+	event.preventDefault();
+	var sliderTarget = $("#left-menu");
+
+	if(sliderTarget.data("clickState") != 1) {
+
+		var sliderTarget = $("#left-menu");
+		sliderTarget.data("clickState", 1).
+			removeClass('collapse').
+			addClass("custom-slider").find('ul');
+
+		$(this).animate({
+			left: "+=" + sliderTarget.css("width")
+		});
+	}
+}
+
+function swipeleftHandler(event){
+	event.preventDefault();
+	var sliderTarget = $("#left-menu");
+	if(sliderTarget.data("clickState") == 1) {
+		var sliderTarget = $("#left-menu");
+		$(this).animate({
+				left: "-=" + sliderTarget.css("width")
+			}, function() {
+				sliderTarget.data("clickState", 0).
+					addClass('collapse').
+					removeClass("custom-slider");
+		});
+	}
+}
+
+// Bind the swipeleftHandler callback function to the swipe event on div.box
+$( ".container" ).on("swipeleft", swipeleftHandler).on("swiperight", swiperightHandler);
